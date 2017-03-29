0a2eae42102e5c056cfe99f7728b1cbccb4b2e17
Nick <nickfoden@gmail.com>
2017-03-29 13:40:09 -0400

    linked sheets and added basic view

---
+++ app.js
@@ -0,0 +1,21 @@
+var playerOne = {
+	one: false;
+	two: false;
+	three: false;
+	four: false;
+	five: false;
+	six: false
+	seven: false; 
+	eight: false; 
+	nine: false;
+}
+var PlayerTwo = {
+		one: false;
+	two: false;
+	three: false;
+	four: false;
+	five: false;
+	six: false
+	seven: false; 
+	eight: false
+}
