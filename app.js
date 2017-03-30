861672460797c9999738991cc9d2bb7a048c4dd7
Nick <nickfoden@gmail.com>
2017-03-30 15:05:08 -0400

    Grading works thanks to some if statements

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var youWinTwo = "PLAYER TWO WINS!";
 $('.cell').click(function(){
 	var id = $(this).attr('id');
 	id = parseInt(id);
-	console.log(id);
 	if (gamePlay[id] == 0) { 
 		if (currentPlayer == 1) {
 			$(this).toggleClass('playerOne');
@@ -29,16 +28,47 @@ $('.cell').click(function(){
 })
 
 function grade(){
-	 var game = JSON.stringify(gamePlay);
-	if (game === JSON.stringify([1,1,1,2,2,0,0,0,0]) || game === [2,2,2,1,1,1,1,1] || game === [1,1,1,2,2,1,1,1,1]){
-		$(".winner").html(youWinOne)
+	var gradeGame = [ [gamePlay[0], gamePlay[1], gamePlay[2]], [gamePlay[3], gamePlay[4], gamePlay[5]], [gamePlay[6], gamePlay[7], gamePlay[8]] ];
+	for (var i = 0; i<=2; i++) {
+		if (gradeGame[0][i] === gradeGame[1][i] && gradeGame[0][i] === gradeGame[2][i]) {
+			if (gradeGame[0][i] === 1) {
+				$(".winner").html(youWinOne)
+			}
+			else if (gradeGame[0][i] === 2) {
+				$(".winner").html(youWinTwo);
+			}
+			console.log('winner on column: ' + i)
 		}
-	else if (game === [2,2,2,1,1,0,1,0,0] || game === [1,1,1,2,2,2,1,1,1] || game === [1,1,1,2,2,2,1,1,1]) {
-		$(".winner").html(youWinTwo);
+	};
+	for (var i = 0; i<=2; i++) {
+		if (gradeGame[i][0] === gradeGame[i][1] && gradeGame[i][0] === gradeGame[i][2]) {
+			if (gradeGame[i][0] === 1) {
+				$(".winner").html(youWinOne)
+			}
+			else if (gradeGame[i][0] === 2) {
+				$(".winner").html(youWinTwo);
+			}
+			console.log('winner on row: ' + i)
 		}
-	else{
-
+	};
+	if (gradeGame[0][0] === gradeGame[1][1] && gradeGame[0][0] === gradeGame[2][2]) {
+		if (gradeGame[0][0] === 1) {
+			$(".winner").html(youWinOne)
 		}
+		else if (gradeGame[0][0] === 2) {
+			$(".winner").html(youWinTwo);
+		}
+	};
+	if (gradeGame[0][2] === gradeGame[1][1] && gradeGame[0][2] === gradeGame[2][0]) {
+		if (gradeGame[0][2] === 1) {
+			$(".winner").html(youWinOne)
+		}
+		else if (gradeGame[0][2] === 2) {
+			$(".winner").html(youWinTwo);
+		}
+	};
+		
+	
 };
-
+	
 });
