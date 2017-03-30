b39555e08f35f06f63281f38160a15ff33ee1b6b
Nick Foden <nickfoden@gmail.com>
2017-03-29 23:53:53 -0400

    Well you can't comapre two arrays without JSON stringify

--- app.js
+++ app.js
@@ -2,39 +2,38 @@ $(document).ready(function(){
 
 "use strict";
 
-var gamePlay = [1,1,1,1,1,1,1,1,1];
+var gamePlay = [0,0,0,0,0,0,0,0,0];
 
 var currentPlayer = 1;
 
 var youWinOne = "PLAYER ONE WINS!";
 var youWinTwo = "PLAYER TWO WINS!";
-
-function playGame(){ 
-	$('.cell').click(function(){
-		var id = $(this).attr('id');
-		parseInt(id);
-		console.log(id);
-		if (gamePlay[id] == 1) { 
-			if (currentPlayer == 1) {
+ 
+$('.cell').click(function(){
+	var id = $(this).attr('id');
+	id = parseInt(id);
+	console.log(id);
+	if (gamePlay[id] == 0) { 
+		if (currentPlayer == 1) {
 			$(this).toggleClass('playerOne');
-			gamePlay[id] = 2;
+			gamePlay[id] = 1;
 			currentPlayer = 2;
-			}
-			else {
+		}
+		else {
 			$(this).toggleClass('playerTwo');
-			gamePlay[id] = 3;
+			gamePlay[id] = 2;
 			currentPlayer = 1;
-				}
-			}
-	})
+		}
+	}
 	grade();
-};
+})
 
 function grade(){
-	if (gamePlay == [2,2,2,3,3,1,1,1,1] || [3,3,3,2,2,2,1,1,1] || [1,1,1,2,2,2,3,3,3]){
+	 var game = JSON.stringify(gamePlay);
+	if (game === JSON.stringify([1,1,1,2,2,0,0,0,0]) || game === [2,2,2,1,1,1,1,1] || game === [1,1,1,2,2,1,1,1,1]){
 		$(".winner").html(youWinOne)
 		}
-	else if (gamePlay == [3,3,3,2,1,2,1,2,1] || [2,1,2,3,3,3,1,2,1]) {
+	else if (game === [2,2,2,1,1,0,1,0,0] || game === [1,1,1,2,2,2,1,1,1] || game === [1,1,1,2,2,2,1,1,1]) {
 		$(".winner").html(youWinTwo);
 		}
 	else{
@@ -42,156 +41,4 @@ function grade(){
 		}
 };
 
-playGame();
-
 });
-
-/*function gameOn(){ 
-	$(".cell").click(function(){
-		if (currentPlayer == 1) {
-		$(this).toggleClass('playerOne');
-			currentPlayer = 2;
-		}
-		else {
-		$(this).toggleClass('playerTwo');
-		currentPlayer =1;
-		}
-
-	});
-};
-
-gameOn();*/
-
-/*var playerX ={
-	A: "no",
-	B: "no",
-	C: "no",
-	D: "no",
-	E: "no",
-	F: "no",
-	G: "no",
-	H: "no",
-	I: "no",
-};
-//playerOne
-var playerY ={
-	A: "no",
-	B: "no",
-	C: "no",
-	D: "no",
-	E: "no",
-	F: "no",
-	G: "no",
-	H: "no",
-	I: "no",
-};
-//playerTwo
-*/
-
-/*
-function gameOn(){
-$('#A').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-$('#B').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-$('#C').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-$('#D').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-$('#E').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-$('#F').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-$('#G').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-
-});
-
-$('#H').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-$('#I').click(function(){
-	if (currentPlayer == 1) {
-	$(this).toggleClass('playerOne');
-	currentPlayer = 2;
-	}
-	else {
-	$(this).toggleClass('playerTwo');
-	currentPlayer =1;
-	}
-});
-
-}*/
-
-/*$('.cell').click(function(){
-	$(this).toggleClass('playerOne');
-});*/
