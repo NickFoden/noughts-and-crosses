9a723c15d5716c6c36d2da444d1c37e254576763
Nick <nickfoden@gmail.com>
2017-03-30 15:43:13 -0400

    cleaned up code, added refresh link to title, working pretty good

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
-
 "use strict";
-
 var gamePlay = [0,0,0,0,0,0,0,0,0];
-
 var currentPlayer = 1;
-
 var youWinOne = "PLAYER ONE WINS!";
 var youWinTwo = "PLAYER TWO WINS!";
  
@@ -29,46 +25,45 @@ $('.cell').click(function(){
 
 function grade(){
 	var gradeGame = [ [gamePlay[0], gamePlay[1], gamePlay[2]], [gamePlay[3], gamePlay[4], gamePlay[5]], [gamePlay[6], gamePlay[7], gamePlay[8]] ];
-	for (var i = 0; i<=2; i++) {
-		if (gradeGame[0][i] === gradeGame[1][i] && gradeGame[0][i] === gradeGame[2][i]) {
-			if (gradeGame[0][i] === 1) {
-				$(".winner").html(youWinOne)
+		for (var i = 0; i<=2; i++) {
+			//columns
+			if (gradeGame[0][i] === gradeGame[1][i] && gradeGame[0][i] === gradeGame[2][i]) {
+				if (gradeGame[0][i] === 1) {
+					$(".winner").html(youWinOne);
+				}
+				else if (gradeGame[0][i] === 2) {
+					$(".winner").html(youWinTwo);
+				}
 			}
-			else if (gradeGame[0][i] === 2) {
+		};
+		for (var i = 0; i<=2; i++) {
+			//rows
+			if (gradeGame[i][0] === gradeGame[i][1] && gradeGame[i][0] === gradeGame[i][2]) {
+				if (gradeGame[i][0] === 1) {
+					$(".winner").html(youWinOne);
+				}
+				else if (gradeGame[i][0] === 2) {
+					$(".winner").html(youWinTwo);
+				}
+			}
+		};
+		if (gradeGame[0][0] === gradeGame[1][1] && gradeGame[0][0] === gradeGame[2][2]) {
+			//diagnols
+			if (gradeGame[0][0] === 1) {
+				$(".winner").html(youWinOne);
+			}
+			else if (gradeGame[0][0] === 2) {
 				$(".winner").html(youWinTwo);
 			}
-			console.log('winner on column: ' + i)
-		}
-	};
-	for (var i = 0; i<=2; i++) {
-		if (gradeGame[i][0] === gradeGame[i][1] && gradeGame[i][0] === gradeGame[i][2]) {
-			if (gradeGame[i][0] === 1) {
-				$(".winner").html(youWinOne)
+		};
+		if (gradeGame[0][2] === gradeGame[1][1] && gradeGame[0][2] === gradeGame[2][0]) {
+			//diagnols
+			if (gradeGame[0][2] === 1) {
+				$(".winner").html(youWinOne);
 			}
-			else if (gradeGame[i][0] === 2) {
+			else if (gradeGame[0][2] === 2) {
 				$(".winner").html(youWinTwo);
 			}
-			console.log('winner on row: ' + i)
-		}
+		};
 	};
-	if (gradeGame[0][0] === gradeGame[1][1] && gradeGame[0][0] === gradeGame[2][2]) {
-		if (gradeGame[0][0] === 1) {
-			$(".winner").html(youWinOne)
-		}
-		else if (gradeGame[0][0] === 2) {
-			$(".winner").html(youWinTwo);
-		}
-	};
-	if (gradeGame[0][2] === gradeGame[1][1] && gradeGame[0][2] === gradeGame[2][0]) {
-		if (gradeGame[0][2] === 1) {
-			$(".winner").html(youWinOne)
-		}
-		else if (gradeGame[0][2] === 2) {
-			$(".winner").html(youWinTwo);
-		}
-	};
-		
-	
-};
-	
 });
